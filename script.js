6ee63466d63c8ba96a03c530c635948d493d3b29
Remaker00 <93196599+Remaker00@users.noreply.github.com>
2023-07-26 10:44:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+var itm = document.getElementsByClassName('list-group');
+
+for(var i=0;i<itm.length;i++){
+    itm[i].style.color = 'red';
+}
+
+var Li= document.getElementsByTagName("li");
+Li[1].style.fontWeight="bold";
+
